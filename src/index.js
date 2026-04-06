1fd68878b8986871c8471335c8cd45e56018b45e
Caeson Daniell <239234311+caesondaniell@users.noreply.github.com>
2026-04-06 12:05:45 -0700

    Add feature: weather check checkWeather() will return current weather data from Visual Crossing API
    Future commits:
    |Features:
    |-Render weather info
    |
    |Fixes:
    |-
    |
    |Other:
    |-split into modules

---
+++ src/index.js
@@ -0,0 +1,46 @@
+import './style.css'
+
+async function getInfo (city, unit) {
+  try {
+    const raw = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=${unit}&iconSet=icons2&key=UCRLKHMNXE8RGJB3Q5XPAW3AK&contentType=json`
+    )
+    const info = await raw.json()
+    return info
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+async function checkWeather (city, unit) {
+  const deg = unit === 'f' ? 'us' : 'metric'
+  const {
+    currentConditions: current
+    , days
+    , resolvedAddress: location
+  } = await getInfo(city, deg)
+  const {
+    conditions
+    , feelslike
+    , icon
+    , precipprob
+    , temp
+    , winddir
+    , windgust
+    , windspeed
+  } = current
+  const { tempmax: max, tempmin: min } = days[0]
+  return {
+    location
+    , conditions
+    , feelslike
+    , icon
+    , precipprob
+    , temp
+    , max
+    , min
+    , winddir
+    , windgust
+    , windspeed
+  }
+}
